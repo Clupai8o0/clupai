4f03d360b09afbb85a8f34a2c68a6e1b29f07cc1
Clupai8o0 <clupaio4@gmail.com>
2023-03-19 13:50:36 +0300

    updates to the portfolio construction design

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,25 @@
-import React from "react";
+import Head from "next/head";
 
 function Home() {
 	return (
-		<div className="w-full h-[100vh] flex justify-center items-center bg-gray-900">
-			<h1 className="font-sans text-4xl text-white tracking-wider">Under Construction...</h1>
-		</div>
+		<>
+			<Head>
+				<link rel="shortcut icon" href="/splash.png" type="image/x-icon" />
+				<title>Sorry...</title>
+				<meta http-equiv="X-UA-Compatible" content="ie=edge" />
+				<meta
+					name="description"
+					content="My bad mate 🙏, the portfolio is still not completed"
+				/>
+				<meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+			</Head>
+			<div className="w-full h-[100vh] flex flex-col justify-center items-center bg-[#131313] p-4">
+				<img src="/construction.svg" alt="house construction hero image" className="md:w-[600px]" />
+				<h1 className="font-sans text-2xl md:text-4xl xl:text-5xl font-semibold text-white mt-8 md:mt-12 xl:mt-16 text-center">
+					Sorry, it's under construction...
+				</h1>
+			</div>
+		</>
 	);
 }
 
